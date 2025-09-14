d3b8abf4dee8bfc62d463d2ebb2cf4996f38f6c4
Janne Valkealahti <janne.valkealahti@gmail.com>
2025-09-14 12:32:42 +0100

    Move apiserver controller under different path

--- sample/apiserver/src/main/java/com/github/jvalkeal/secpat/apiserver/ApiServerWhoamiController.java
+++ sample/apiserver/src/main/java/com/github/jvalkeal/secpat/apiserver/ApiServerWhoamiController.java
@@ -4,22 +4,20 @@ import java.security.Principal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/api")
-public class ApiServerController {
+@RequestMapping(path = "/api/whoami")
+public class ApiServerWhoamiController {
 
-	private final static Logger log = LoggerFactory.getLogger(ApiServerController.class);
+	private final static Logger log = LoggerFactory.getLogger(ApiServerWhoamiController.class);
 
-	@GetMapping("/whoami1")
+	@GetMapping("/authenticationprincipal")
 	String whoami1(@AuthenticationPrincipal Object user) {
 		String token = "";
 		if (user instanceof DefaultOidcUser duser) {
@@ -31,7 +29,7 @@ public class ApiServerController {
 		return String.format("%s - %s - %s", user != null ? user.getClass() : null, user, token);
 	}
 
-	@GetMapping("/whoami2")
+	@GetMapping("/principal")
 	String whoami2(Principal user) {
 
 		String token = "";
@@ -41,30 +39,8 @@ public class ApiServerController {
 		return String.format("%s - %s - %s", user != null ? user.getClass() : null, user, token);
 	}
 
-	@GetMapping("/whoami3")
+	@GetMapping("/authentication")
 	String whoami3(Authentication user) {
-		log.debug("User Class {}", user != null ? user.getClass() : null);
-		log.debug("User {}", user);
-		return String.format("%s - %s", user != null ? user.getClass() : null, user);
-	}
-
-	@GetMapping("/whoami4")
-	String whoami4(@AuthenticationPrincipal User user) {
-		log.debug("User Class {}", user != null ? user.getClass() : null);
-		log.debug("User {}", user);
-		return String.format("%s - %s", user != null ? user.getClass() : null, user);
-	}
-
-	@GetMapping("/read")
-	@Secured("read")
-	String read(Authentication user) {
-		log.debug("User Class {}", user != null ? user.getClass() : null);
-		log.debug("User {}", user);
-		return String.format("%s - %s", user != null ? user.getClass() : null, user);
-	}
-
-	@GetMapping("/write")
-	String write(Authentication user) {
 		log.debug("User Class {}", user != null ? user.getClass() : null);
 		log.debug("User {}", user);
 		return String.format("%s - %s", user != null ? user.getClass() : null, user);
